3428adbd2c9edc6f2b3e2c1d634f55fc2c8530da
mzza <1072793927@qq.com>
2017-08-14 10:41:52 +0800

    add myne network

---
+++ MNIST/network_myself.py
@@ -0,0 +1,197 @@
+'''
+this is the code for recgnition of MNIST with DNN
+
+reference:Neural Network and Deep Learning
+
+2017.8.11
+'''
+from __future__ import division
+import numpy as np
+import math
+import random
+######################
+
+def weigth_init(input_dim,h,output_dim):
+	w1=np.random.normal(0,1/math.sqrt(input_dim),(input_dim,h))
+	w2=np.random.normal(0,1/math.sqrt(h),(h,output_dim))
+	b1=np.random.randn(1,h)
+	b2=np.random.randn(1,output_dim)
+	return w1,w2,b1,b2
+
+def sigmoid(x):
+	return 1/(1+np.exp(-x))
+
+def div_sigmoid(x):
+	return sigmoid(x)*(1-sigmoid(x))
+
+def label_trans(train_label,output_dim):
+	label=np.zeros((train_label.shape[0],output_dim))
+	for i in range(label.shape[0]):
+		label[i,train_label[i]]=1
+	return label
+
+def inference(X,w1,w2,b1,b2):
+	''' forward progation
+
+		inputs: 
+		X: the input data
+		returns:
+		z: output of the network
+	'''
+	net1=np.dot(X,w1)+b1
+	y=sigmoid(net1)
+	net2=np.dot(y,w2)+b2
+	z=sigmoid(net2)
+
+	return y,z,net1,net2
+
+def back_propagation(X,label,w1,w2,b1,b2,eta):
+	y,z,net1,net2=inference(X,w1,w2,b1,b2)
+	deta1=(z-label)*div_sigmoid(net2)
+	delta_w2=np.dot(y.T,deta1)
+
+	w2=w2-delta_w2*eta
+	b2=b2-deta1*eta
+
+	y,z,net1,net2=inference(X,w1,w2,b1,b2)
+	deta1=(z-label)*div_sigmoid(net2)
+	deta2=np.dot(deta1,w2.T)*div_sigmoid(net1)
+	delta_w1=np.dot(X.T,deta2)
+
+	return deta1,delta_w2,deta2,delta_w1
+
+def online_train(train_data,train_label,w1,w2,b1,b2,eta):
+	for i in range(train_data.shape[0]):
+		deta1,delta_w2,deta2,delta_w1=back_propagation(train_data[i,:].reshape((1,-1)),train_label[i,:].reshape((1,-1)),w1,w2,b1,b2,eta)
+		w2=w2-delta_w2*eta
+		b2=b2-deta1*eta
+		w1=w1-delta_w1*eta
+		b1=b1-deta2*eta
+
+		if (i+1)%5000==0:
+			print("traing times:%d"%(i+1))
+
+	return w1,w2,b1,b2
+
+def batch_trian(train_data,train_label,w1,w2,b1,b2,eta):
+	deta1=np.zeros((train_data.shape[0],b2.shape[1]))
+	deta2=np.zeros((train_data.shape[0],b1.shape[1]))
+	delta_w1=np.zeros((w1.shape[0],w1.shape[1],train_data.shape[0]))
+	delta_w2=np.zeros((w2.shape[0],w2.shape[1],train_data.shape[0]))
+
+	for i in range(train_data.shape[0]):
+		deta1[i,:],delta_w2[:,:,i],deta2[i,:],delta_w1[:,:,i]=back_propagation(train_data[i,:].reshape((1,-1)),train_label[i,:].reshape((1,-1)),w1,w2,b1,b2,eta)
+
+	deta1=deta1.sum(axis=0)
+	deta2=deta2.sum(axis=0)
+	delta_w1=delta_w1.sum(axis=2)
+	delta_w2=delta_w2.sum(axis=2)
+
+	w2=w2-delta_w2*eta
+	b2=b2-deta1*eta
+	w1=w1-delta_w1*eta
+	b1=b1-deta2*eta
+
+	return w1,w2,b1,b2
+
+def mini_batch_train(train_data,train_label,w1,w2,b1,b2,eta,mini_batch_size):
+	mini_batch_index=range(train_data.shape[0])
+	random.shuffle(mini_batch_index)
+	mini_batch_index=np.array(mini_batch_index).reshape((-1,mini_batch_size))
+
+	deta1=np.zeros((mini_batch_size,b2.shape[1]))
+	deta2=np.zeros((mini_batch_size,b1.shape[1]))
+	delta_w1=np.zeros((w1.shape[0],w1.shape[1],mini_batch_size))
+	delta_w2=np.zeros((w2.shape[0],w2.shape[1],mini_batch_size))
+
+	for k in range(mini_batch_index.shape[0]):
+		w1,w2,b1,b2=batch_trian(train_data[mini_batch_index[k,:]],train_label[mini_batch_index[k,:]],w1,w2,b1,b2,eta)
+
+		if (k+1)%1000==0:
+			print("mini_batch trianing:%d"%(k+1))
+	return w1,w2,b1,b2
+
+def trian(train_data,train_label,input_dim,h,output_dim,eta,trian_model):
+	train_label=label_trans(train_label,output_dim)
+	w1,w2,b1,b2=weigth_init(input_dim,h,output_dim)
+	if trian_model=='online':
+		epoch=5
+		for i in range(epoch):
+			w1,w2,b1,b2=online_train(train_data,train_label,w1,w2,b1,b2,eta)
+			print("the training epoch:%d"%(i+1))
+		return w1,w2,b1,b2
+
+	elif trian_model=='batch':
+		epoch=5
+		for i in range(epoch):
+			w1,w2,b1,b2=batch_train(train_data,train_label,w1,w2,b1,b2,eta)
+			print("the training epoch:%d"%(i+1))
+		return w1,w2,b1,b2
+	else :
+		epoch=5
+		mini_batch_size=10
+		for i in range(epoch):
+			w1,w2,b1,b2=mini_batch_train(train_data,train_label,w1,w2,b1,b2,eta,mini_batch_size)
+			print("the training epoch:%d"%(i+1))
+		return w1,w2,b1,b2
+
+class Network(object):
+	"""the easiest version of deep Neural network
+
+		Parameters:
+		------------
+		h: int
+			the number of the hidden layer neurals
+		input_dim : int
+			the demension of input data
+		output_dim : int
+			the dimension of out put
+		eta: float
+			learn rate
+		theta :float,a very small number
+			the stop criteria
+		train_model: ['batch','mini_batch','online']
+
+		Atributte:
+		----------
+		w1,w2,b1,b2
+	"""
+	def __init__(self, input_dim,h,output_dim,eta,trian_model):
+		self.input_dim=input_dim
+		self.output_dim=output_dim
+		self.h=h
+		self.eta=eta
+		self.trian_model=trian_model
+
+	def fit(self,train_data,train_label):
+		''' train the network
+
+			Input:
+			train :matrix ,[training_data,features],the training data
+			train_label :vector,[label_1,....label_2],the label of the traing data
+
+			return: self
+		'''
+		self.w1,self.w2,self.b1,self.b2=trian(train_data,train_label,self.input_dim,self.h,self.output_dim,self.eta,self.trian_model)
+
+	def predict(self,test_data,test_label):
+		'''calulate the accuracy on test data
+			
+			Inputs:
+			test: matrix
+			test_label :vector
+
+			Outputs:correct_rate
+		'''
+		y,z,net1,net2=inference(test_data,self.w1,self.w2,self.b1,self.b2)
+		predict_label=np.argmax(z,axis=1)
+		flag=(predict_label==test_label)
+		return flag.sum()/test_label.size
+
+
+		
+
+
+                
+
+        
--- MNIST/setup.py
+++ MNIST/setup.py
@@ -3,6 +3,6 @@ import network
 get_data.load_data('mnist.pkl')
 train_data,validation_data,test_data=get_data.load_data('mnist.pkl')
 
-clf=network.Network(train_data[0].shape[1],20,10,1,1,'times')
+clf=network.Network(train_data[0].shape[1],20,10,1,'mini_batch')
 clf.fit(train_data[0],train_data[1])   
 accurate_rate=clf.predict(test_data[0],test_data[1])
